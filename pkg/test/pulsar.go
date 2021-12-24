c88e4400e49f7746de4d4155804bf1d9d0bc4945
ZhangJian He <shoothzj@gmail.com>
2021-12-24 18:48:25 +0800

    add test pulsar method (#1)

---
+++ pkg/test/pulsar.go
@@ -0,0 +1,119 @@
+package test
+
+import (
+	"context"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+)
+
+const (
+	pulsarContainerName = "mqtt-test-pulsar"
+)
+
+func setupPulsar() {
+	var once sync.Once
+	now := time.Now()
+	for {
+		if time.Since(now).Minutes() > 3 {
+			panic("pulsar still not started")
+		}
+		resp, err := http.Get("http://localhost:8080/admin/v2/brokers/health")
+		if err != nil {
+			logrus.Error("connect pulsar error ")
+			once.Do(startPulsar)
+			time.Sleep(15 * time.Second)
+			continue
+		}
+		if resp.StatusCode == 200 {
+			logrus.Info("health check success")
+			break
+		}
+		logrus.Info("resp code is ", resp.StatusCode)
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func startPulsar() {
+	logrus.Info("start pulsar container")
+	err := startPulsarInternal()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func startPulsarInternal() error {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return err
+	}
+	portSpecs, bindings, err := nat.ParsePortSpecs([]string{"6650", "8080"})
+	logrus.Info("port binding is ", bindings)
+	resp, err := cli.ContainerCreate(context.TODO(), &container.Config{
+		Image:        "ttbb/pulsar:mate",
+		Env:          []string{"REMOTE_MODE=false"},
+		ExposedPorts: portSpecs,
+		Tty:          false,
+	}, &container.HostConfig{
+		PortBindings: nat.PortMap{
+			"6650/tcp": []nat.PortBinding{
+				{
+					HostIP:   "0.0.0.0",
+					HostPort: "6650",
+				},
+			},
+			"8080/tcp": []nat.PortBinding{
+				{
+					HostIP:   "0.0.0.0",
+					HostPort: "8080",
+				},
+			},
+		},
+	}, nil, nil, "mqtt-test-pulsar")
+	if err != nil {
+		panic(err)
+	}
+
+	return cli.ContainerStart(context.TODO(), resp.ID, types.ContainerStartOptions{})
+}
+
+func stopPulsar() {
+	logrus.Info("stop pulsar container")
+	err := stopPulsarInternal()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func stopPulsarInternal() error {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return err
+	}
+	containerList, err := cli.ContainerList(context.TODO(), types.ContainerListOptions{})
+	if err != nil {
+		return err
+	}
+	for _, c := range containerList {
+		names := c.Names
+		for _, name := range names {
+			if strings.Contains(name, pulsarContainerName) {
+				duration := 3 * time.Minute
+				err := cli.ContainerStop(context.TODO(), c.ID, &duration)
+				if err != nil {
+					return err
+				}
+				_ = cli.ContainerRemove(context.TODO(), c.ID, types.ContainerRemoveOptions{
+					Force: true,
+				})
+			}
+		}
+	}
+	return nil
+}
