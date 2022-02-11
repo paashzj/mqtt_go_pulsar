7ed3d2b807fe7a0e3b734aa6022042206f35ddb3
goflutterjava <goflutterjava@gmail.com>
2022-02-11 14:35:59 +0800

    Extract method from bridge mq

--- pkg/mqsar/bridge_mq.go
+++ pkg/mqsar/bridge_mq.go
@@ -78,155 +78,45 @@ func (p *pulsarBridgeMq) Publish(e *bridge.Elements) error {
 		ClientId: e.ClientID,
 	}
 	if e.Action == bridge.Connect {
-		p.mutex.Lock()
-		consumerKey := p.sessionConsumerMap[mqttSessionKey]
-		if len(consumerKey) != 0 {
-			for _, value := range consumerKey {
-				p.closeConsumer(value)
-			}
-		}
-		producerKey := p.sessionProducerMap[mqttSessionKey]
-		if len(producerKey) != 0 {
-			for _, value := range producerKey {
-				p.closeProducer(value)
-			}
-		}
-		p.sessionProducerMap[mqttSessionKey] = make([]module.MqttTopicKey, 0)
-		p.sessionConsumerMap[mqttSessionKey] = make([]module.MqttTopicKey, 0)
-		p.mutex.Unlock()
+		p.handleConnect(mqttSessionKey)
 	} else if e.Action == bridge.Disconnect {
-		p.mutex.Lock()
-		// no topic information when close session
-		p.closeSession(mqttSessionKey)
-		p.mutex.Unlock()
+		p.handleDisconnect(mqttSessionKey)
 	} else if e.Action == bridge.Subscribe {
-		mqttTopicKey := module.MqttTopicKey{
-			MqttSessionKey: mqttSessionKey,
-			Topic:          e.Topic,
-		}
-		p.mutex.Lock()
-		consumeTopic, err := p.server.MqttConsumeTopic(e.Username, e.ClientID, e.Topic)
-		if err != nil {
-			logrus.Error("get consumer topic failed ", err)
-			return nil
-		} else {
-			consumeOptions := pulsar.ConsumerOptions{}
-			consumeOptions.Type = pulsar.Shared
-			consumeOptions.Topic = consumeTopic
-			consumeOptions.SubscriptionName = e.Username
-			consumer, err := p.pulsarClient.Subscribe(consumeOptions)
-			if err != nil {
-				logrus.Error("create consumer failed ", err)
-				return nil
-			} else {
-				p.consumerMap[mqttTopicKey] = consumer
-				routineContext := consume.StartConsumeRoutine(mqttTopicKey, consumer)
-				p.consumerRoutineContextMap[mqttTopicKey] = routineContext
-				p.sessionConsumerMap[mqttSessionKey] = append(p.sessionConsumerMap[mqttSessionKey], mqttTopicKey)
-			}
-		}
-		p.mutex.Unlock()
+		return p.handleSubscribe(e, mqttSessionKey)
 	} else if e.Action == bridge.Unsubscribe {
-		logrus.Infof("begin to unsubscribe mqtt topic: %s", e.Topic)
-		mqttTopicKey := module.MqttTopicKey{
-			MqttSessionKey: mqttSessionKey,
-			Topic:          e.Topic,
-		}
-		p.mutex.Lock()
-		p.closeConsumer(mqttTopicKey)
-		p.mutex.Unlock()
+		p.handleUnsubscribe(e, mqttSessionKey)
 	} else if e.Action == bridge.Publish {
-		mqttTopicKey := module.MqttTopicKey{
-			MqttSessionKey: mqttSessionKey,
-			Topic:          e.Topic,
-		}
-		p.mutex.RLock()
-		aux := p.producerMap[mqttTopicKey]
-		p.mutex.RUnlock()
-		if aux == nil {
-			produceTopic, err := p.server.MqttProduceTopic(e.Username, e.ClientID, e.Topic)
-			if err != nil {
-				logrus.Error("get produce topic failed ", err)
-				return nil
-			} else {
-				producerOptions := pulsar.ProducerOptions{}
-				producerOptions.DisableBatching = p.pulsarConfig.ProduceConfig.DisableBatching
-				producerOptions.SendTimeout = p.pulsarConfig.ProduceConfig.SendTimeout
-				producerOptions.BatchingMaxPublishDelay = p.pulsarConfig.ProduceConfig.BatchingMaxPublishDelay
-				producerOptions.MaxPendingMessages = p.pulsarConfig.ProduceConfig.MaxPendingMessages
-				producerOptions.DisableBlockIfQueueFull = true
-				producerOptions.Topic = produceTopic
-				logrus.Infof("begin to create producer. mqttTopic : %s, topic : %s", e.Topic, produceTopic)
-				producer, err := p.pulsarClient.CreateProducer(producerOptions)
-				if err != nil {
-					logrus.Error("create produce failed ", err)
-					return nil
-				} else {
-					p.mutex.Lock()
-					p.producerMap[mqttTopicKey] = producer
-					p.sessionProducerMap[mqttSessionKey] = append(p.sessionProducerMap[mqttSessionKey], mqttTopicKey)
-					p.mutex.Unlock()
-					aux = producer
-				}
-			}
-		}
-		producerMessage := pulsar.ProducerMessage{}
-		producerMessage.Payload = []byte(e.Payload)
-		startTime := time.Now()
-		localSpan, _, spanErr := p.tracer.CreateEntrySpan(context.TODO(), "send-pulsar", func(headerKey string) (string, error) {
-			return "", nil
-		})
-		localSpan.SetSpanLayer(v3.SpanLayer_MQ)
-		if spanErr != nil {
-			logrus.Debug("create span err", spanErr)
-		} else {
-			localSpan.SetOperationName("send pulsar")
-			localSpan.Tag("topic", aux.Topic())
-		}
-		if p.mqttConfig.Qos1NoWaitReply {
-			task := func() {
-				aux.SendAsync(context.TODO(), &producerMessage, func(id pulsar.MessageID, message *pulsar.ProducerMessage, err error) {
-					if spanErr == nil {
-						localSpan.End()
-					}
-					if err != nil {
-						metrics.PulsarSendFailCount.WithLabelValues(e.Topic, aux.Topic()).Add(1)
-						logrus.Error("Send pulsar error ", err)
-					} else {
-						metrics.PulsarSendSuccessCount.WithLabelValues(e.Topic, aux.Topic()).Add(1)
-						logrus.Info("Send pulsar success ", id)
-					}
-					metrics.PulsarSendLatency.WithLabelValues(e.Topic, aux.Topic()).Observe(
-						float64(time.Since(startTime).Milliseconds()))
-				})
-			}
-			if p.pulsarConfig.ProduceConfig.DisableRoutinePool {
-				go task()
-			} else {
-				err := p.pool.Submit(task)
-				if err != nil {
-					logrus.Errorf("submit send pulsar task failed. err: %s", err)
-				}
-			}
-		} else {
-			messageID, err := aux.Send(context.TODO(), &producerMessage)
-			if spanErr == nil {
-				localSpan.End()
-			}
-			if err != nil {
-				metrics.PulsarSendFailCount.WithLabelValues(e.Topic, aux.Topic()).Add(1)
-				logrus.Error("Send pulsar error ", err)
-			} else {
-				metrics.PulsarSendSuccessCount.WithLabelValues(e.Topic, aux.Topic()).Add(1)
-				logrus.Info("Send pulsar success ", messageID)
-			}
-			metrics.PulsarSendLatency.WithLabelValues(e.Topic, aux.Topic()).Observe(
-				float64(time.Since(startTime).Milliseconds()))
-		}
+		return p.handlePublish(e, mqttSessionKey)
 	} else {
 		logrus.Warn("Unsupported action ", e.Action)
 	}
 	return nil
+}
+
+func (p *pulsarBridgeMq) handleConnect(mqttSessionKey module.MqttSessionKey) {
+	p.mutex.Lock()
+	consumerKey := p.sessionConsumerMap[mqttSessionKey]
+	if len(consumerKey) != 0 {
+		for _, value := range consumerKey {
+			p.closeConsumer(value)
+		}
+	}
+	producerKey := p.sessionProducerMap[mqttSessionKey]
+	if len(producerKey) != 0 {
+		for _, value := range producerKey {
+			p.closeProducer(value)
+		}
+	}
+	p.sessionProducerMap[mqttSessionKey] = make([]module.MqttTopicKey, 0)
+	p.sessionConsumerMap[mqttSessionKey] = make([]module.MqttTopicKey, 0)
+	p.mutex.Unlock()
+}
+
+func (p *pulsarBridgeMq) handleDisconnect(mqttSessionKey module.MqttSessionKey) {
+	p.mutex.Lock()
+	// no topic information when close session
+	p.closeSession(mqttSessionKey)
+	p.mutex.Unlock()
 }
 
 func (p *pulsarBridgeMq) closeSession(mqttSessionKey module.MqttSessionKey) {
@@ -241,6 +131,138 @@ func (p *pulsarBridgeMq) closeSession(mqttSessionKey module.MqttSessionKey) {
 		p.closeConsumer(consumer)
 	}
 	p.sessionConsumerMap[mqttSessionKey] = nil
+}
+
+func (p *pulsarBridgeMq) handleSubscribe(e *bridge.Elements, mqttSessionKey module.MqttSessionKey) error {
+	mqttTopicKey := module.MqttTopicKey{
+		MqttSessionKey: mqttSessionKey,
+		Topic:          e.Topic,
+	}
+	p.mutex.Lock()
+	consumeTopic, err := p.server.MqttConsumeTopic(e.Username, e.ClientID, e.Topic)
+	if err != nil {
+		logrus.Error("get consumer topic failed ", err)
+		return err
+	} else {
+		consumeOptions := pulsar.ConsumerOptions{}
+		consumeOptions.Type = pulsar.Shared
+		consumeOptions.Topic = consumeTopic
+		consumeOptions.SubscriptionName = e.Username
+		consumer, err := p.pulsarClient.Subscribe(consumeOptions)
+		if err != nil {
+			logrus.Error("create consumer failed ", err)
+			return err
+		} else {
+			p.consumerMap[mqttTopicKey] = consumer
+			routineContext := consume.StartConsumeRoutine(mqttTopicKey, consumer)
+			p.consumerRoutineContextMap[mqttTopicKey] = routineContext
+			p.sessionConsumerMap[mqttSessionKey] = append(p.sessionConsumerMap[mqttSessionKey], mqttTopicKey)
+		}
+	}
+	p.mutex.Unlock()
+	return nil
+}
+
+func (p *pulsarBridgeMq) handleUnsubscribe(e *bridge.Elements, mqttSessionKey module.MqttSessionKey) {
+	logrus.Infof("begin to unsubscribe mqtt topic: %s", e.Topic)
+	mqttTopicKey := module.MqttTopicKey{
+		MqttSessionKey: mqttSessionKey,
+		Topic:          e.Topic,
+	}
+	p.mutex.Lock()
+	p.closeConsumer(mqttTopicKey)
+	p.mutex.Unlock()
+}
+
+func (p *pulsarBridgeMq) handlePublish(e *bridge.Elements, mqttSessionKey module.MqttSessionKey) error {
+	mqttTopicKey := module.MqttTopicKey{
+		MqttSessionKey: mqttSessionKey,
+		Topic:          e.Topic,
+	}
+	p.mutex.RLock()
+	aux := p.producerMap[mqttTopicKey]
+	p.mutex.RUnlock()
+	if aux == nil {
+		produceTopic, err := p.server.MqttProduceTopic(e.Username, e.ClientID, e.Topic)
+		if err != nil {
+			logrus.Error("get produce topic failed ", err)
+			return err
+		} else {
+			producerOptions := pulsar.ProducerOptions{}
+			producerOptions.DisableBatching = p.pulsarConfig.ProduceConfig.DisableBatching
+			producerOptions.SendTimeout = p.pulsarConfig.ProduceConfig.SendTimeout
+			producerOptions.BatchingMaxPublishDelay = p.pulsarConfig.ProduceConfig.BatchingMaxPublishDelay
+			producerOptions.MaxPendingMessages = p.pulsarConfig.ProduceConfig.MaxPendingMessages
+			producerOptions.DisableBlockIfQueueFull = true
+			producerOptions.Topic = produceTopic
+			logrus.Infof("begin to create producer. mqttTopic : %s, topic : %s", e.Topic, produceTopic)
+			producer, err := p.pulsarClient.CreateProducer(producerOptions)
+			if err != nil {
+				logrus.Error("create produce failed ", err)
+				return err
+			} else {
+				p.mutex.Lock()
+				p.producerMap[mqttTopicKey] = producer
+				p.sessionProducerMap[mqttSessionKey] = append(p.sessionProducerMap[mqttSessionKey], mqttTopicKey)
+				p.mutex.Unlock()
+				aux = producer
+			}
+		}
+	}
+	producerMessage := pulsar.ProducerMessage{}
+	producerMessage.Payload = []byte(e.Payload)
+	startTime := time.Now()
+	localSpan, _, spanErr := p.tracer.CreateEntrySpan(context.TODO(), "send-pulsar", func(headerKey string) (string, error) {
+		return "", nil
+	})
+	localSpan.SetSpanLayer(v3.SpanLayer_MQ)
+	if spanErr != nil {
+		logrus.Debug("create span err", spanErr)
+	} else {
+		localSpan.SetOperationName("send pulsar")
+		localSpan.Tag("topic", aux.Topic())
+	}
+	if p.mqttConfig.Qos1NoWaitReply {
+		task := func() {
+			aux.SendAsync(context.TODO(), &producerMessage, func(id pulsar.MessageID, message *pulsar.ProducerMessage, err error) {
+				if spanErr == nil {
+					localSpan.End()
+				}
+				if err != nil {
+					metrics.PulsarSendFailCount.WithLabelValues(e.Topic, aux.Topic()).Inc()
+					logrus.Error("Send pulsar error ", err)
+				} else {
+					metrics.PulsarSendSuccessCount.WithLabelValues(e.Topic, aux.Topic()).Inc()
+					logrus.Info("Send pulsar success ", id)
+				}
+				metrics.PulsarSendLatency.WithLabelValues(e.Topic, aux.Topic()).Observe(
+					float64(time.Since(startTime).Milliseconds()))
+			})
+		}
+		if p.pulsarConfig.ProduceConfig.DisableRoutinePool {
+			go task()
+		} else {
+			err := p.pool.Submit(task)
+			if err != nil {
+				logrus.Errorf("submit send pulsar task failed. err: %s", err)
+			}
+		}
+	} else {
+		messageID, err := aux.Send(context.TODO(), &producerMessage)
+		if spanErr == nil {
+			localSpan.End()
+		}
+		if err != nil {
+			metrics.PulsarSendFailCount.WithLabelValues(e.Topic, aux.Topic()).Add(1)
+			logrus.Error("Send pulsar error ", err)
+		} else {
+			metrics.PulsarSendSuccessCount.WithLabelValues(e.Topic, aux.Topic()).Add(1)
+			logrus.Info("Send pulsar success ", messageID)
+		}
+		metrics.PulsarSendLatency.WithLabelValues(e.Topic, aux.Topic()).Observe(
+			float64(time.Since(startTime).Milliseconds()))
+	}
+	return nil
 }
 
 func (p *pulsarBridgeMq) closeProducer(mqttTopicKey module.MqttTopicKey) {
